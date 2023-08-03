84b627ca98b237d81008b00147bc9e0e3ef1a920
Gennadiy <renardes@gmail.com>
2023-08-03 07:32:39 +0500

    2023.07.07: комментарии, правки, итоги

--- 2023.07.07/4.py
+++ 2023.07.07/4.py
@@ -1,12 +1,16 @@
 number = int (input ())
+# ПЕРЕИМЕНОВАТЬ: цифра числа — digit
 figure_1 = number//100
 figure_2 = number%100//10
 figure_3 = number%10
+# ИСПРАВИТЬ: нужно справиться за один вызов print()
 print(f'Cумма цифр = {figure_1 + figure_2 + figure_3}')
 print (f'Произведение цифр = { figure_1 * figure_2 * figure_3}')
-
 
 
 # 333
 # Cумма цифр = 9
 # Произведение цифр = 27
+
+
+# ИТОГ: хорошо, но можно лучше — 3/5
